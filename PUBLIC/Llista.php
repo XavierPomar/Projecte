f8f5d860c90d112034298692b679230743ea2e23
XavierPomar <46518899+XavierPomar@users.noreply.github.com>
2020-11-10 13:19:54 +0100

    Add files via upload

---
+++ PUBLIC/Llista.php
@@ -0,0 +1,45 @@
+!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Rural Shisha</title>
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+
+<?php
+include '../CONFIG/configBD.php';
+//create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if($conn -> connect_error){
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT Id, nom FROM product";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0){
+  //output data of each row
+  while($row = $result->fetch_assoc()){
+    ?>
+    <img src="img/<?php echo $row['Id'];?> .jpg" alt="...">
+    <? echo $row['nom'];?>
+    <a href="Detalls.php?codi=<?php echo $row['Id'];?>"><button class="stretched-link">Detalls</button></a>
+    <?php
+  }
+}else{
+  echo " 0 results ";
+}
+$conn->close();
+?>
+
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+</body>
+</html> 
