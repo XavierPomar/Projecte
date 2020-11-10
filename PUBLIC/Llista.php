2ae0ec4c32988c8348002121e89899d1f897403d
XavierPomar <46518899+XavierPomar@users.noreply.github.com>
2020-11-10 14:21:04 +0100

    Update Llista.php

--- PUBLIC/Llista.php
+++ PUBLIC/Llista.php
@@ -25,7 +25,7 @@ if($result->num_rows > 0){
   //output data of each row
   while($row = $result->fetch_assoc()){
     ?>
-    <img src="IMG/<?php echo $row['Id'];?> .jpg" alt="...">
+    <img src="IMG/<?php echo $row['Id'];?>.jpg" alt="...">
     <? echo $row['nom'];?>
     <a href="Detalls.php?codi=<?php echo $row['Id'];?>"><button class="btn btn-primary stretched-link">Detalls</button></a>
     <?php
