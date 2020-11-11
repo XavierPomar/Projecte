e2a8f20fe3aa8456c6d20e3b08f20f50523313bd
XavierPomar <46518899+XavierPomar@users.noreply.github.com>
2020-11-11 13:14:50 +0100

    Add files via upload

---
+++ PUBLIC/Llista1.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Rural Shisha</title>
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+<?php
+include '../CONFIG/configBD.php';
+//create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if($conn -> connect_error){
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, nom FROM product";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0){
+  //output data of each row
+  while($row = $result->fetch_assoc()){
+    ?>
+    <div class="row">
+    <div class="col-sm-4">
+    <div class="card" style="width: 18rem;">
+      <img src="IMG/<?php echo $row['id'];?>.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title"><?php echo $row['nom'];?></h5>
+        <br>
+        <a href="Detalls.php?id=<?php echo $row['id']; ?>"><button class="btn btn-primary stretched-link">Detalls</button></a>
+      </div>
+    </div>
+    </div>
+    </div>
+ <?php
+  }
+}else{
+  echo " 0 results ";
+}
+$conn->close();
+?>
+
+</div>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+</body>
+</html>
